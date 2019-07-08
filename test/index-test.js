4be561d409d73d80914af9efd5adb92dfefdd291
Poppy Puspa Sari <poppy.puspasari@yahoo.co.id>
2019-07-08 14:11:03 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,12 +1,6 @@
 var index = require("../index.js")
 
 
-describe('Name'), () => {
-  it('returns "Susan"', () => {
-    expect(index.name).toEqual('Susan')
-  })
-}
-
 /*
 describe('what-is-a-test', () => {
   describe('Name', () => {
